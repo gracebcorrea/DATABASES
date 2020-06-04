7dff97310d4ab9c70baa3e65b4b7fa75ae5d8d5a
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-04 17:06:03 -0300

    Update classes1.py

--- classes1.py
+++ classes1.py
@@ -1,5 +1,5 @@
 class flight:
-    def __init__(self, origin, destination, duration)
+    def __init__(self, origin, destination, duration):
         self.origin = origin
         self.destination = destination
         self.duration = duration
