df2e00c69749946e7d335c051f6276bee750b345
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-04 18:05:13 -0300

    Update classes1.py

--- classes1.py
+++ classes1.py
@@ -1,19 +1,23 @@
-class flight:
+class Flight:
+
     def __init__(self, origin, destination, duration):
         self.origin = origin
         self.destination = destination
         self.duration = duration
 
-def main():
-    #create flight
-    f = flight(origin="New York", destination="Paris", duration="540")
-    #change the value of variable
-    f = duration += 10
 
-#print details about flight
-print(f.origin)
-print(f.destination)
-print(f.duration)
+def main():
+
+    # Create flight.
+    f = Flight(origin="New York", destination="Paris", duration=540)
+
+    # Change the value of a variable.
+    f.duration += 10
+
+    # Print details about flight.
+    print(f.origin)
+    print(f.destination)
+    print(f.duration)
 
 if __name__ == "__main__":
     main()
