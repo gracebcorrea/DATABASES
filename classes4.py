0cd0fa7685bbba6640da583279c7194be832b91d
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-04 17:06:16 -0300

    Create classes4.py

---
+++ classes4.py
@@ -0,0 +1,26 @@
+class flight:
+
+    counter = 1
+
+    def __init__(self, origin, destination, duration):
+
+
+        self.origin = origin
+        self.destination = destination
+        self.duration = duration
+
+def print_info(self):
+    print(f"Flight Origin: {self.origin} ")
+    print(f"Flight Destination: {self.Destination} ")
+    print(f"Flight Duration: {self.Duration} minutes ")
+
+def delay(self, amount):
+    self.duration += amount
+
+def main():
+    f1 = Flight(origin="New York", destination="Paris", duration=540)
+    f1.delay()
+    f1.print_info()
+
+if __name__ = "__main__":
+    main()
