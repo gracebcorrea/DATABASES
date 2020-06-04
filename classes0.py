43d04292b29c8b1577e8bff3b014382f23db0a4b
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-04 18:05:09 -0300

    Update classes0.py

--- classes0.py
+++ classes0.py
@@ -1,16 +1,6 @@
-class flight:
+class Flight:
+
     def __init__(self, origin, destination, duration):
         self.origin = origin
         self.destination = destination
         self.duration = duration
-
-      # assume same __init__ method
-
-    def print_info(self):
-          print(f"Flight origin: {self.origin}")
-          print(f"Flight destination: {self.destination}")
-          print(f"Flight duration: {self.duration}")
-
-    def main():
-        f1 = Flight(origin="New York", destination="Paris", duration=540)
-        f1.print_info()
